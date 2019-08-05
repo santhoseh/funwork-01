40557ad1e10dbfb28dc9768d2c83077dfc9fc523
santhoseh <santhosekarthi89@gmail.com>
2019-08-05 17:16:10 +0530

    funwork1

---
+++ main.js
@@ -0,0 +1,159 @@
+SC.initialize({
+client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+//minsara kanavu
+$(document).ready(function() {
+SC.stream('/tracks/574972743',function(sound){
+$("#start").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+
+
+$("#stop").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
+
+//aravic kadaloram
+$(document).ready(function() {
+SC.stream('/tracks/524553387',function(sound){
+$("#start1").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop1").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
+
+//pooraaaleeee ponnuthajeee
+$(document).ready(function() {
+SC.stream('/tracks/319293252',function(sound){
+$("#start2").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop2").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
+//bombay hamma hamma
+$(document).ready(function() {
+SC.stream('/tracks/390797712',function(sound){
+$("#start3").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop3").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
+//rhythm
+$(document).ready(function() {
+SC.stream('/tracks/364718444',function(sound){
+$("#start4").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop4").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});//kadal
+$(document).ready(function() {
+SC.stream('/tracks/66498404',function(sound){
+$("#start5").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop5").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
+//roja
+$(document).ready(function() {
+SC.stream('/tracks/345472022',function(sound){
+$("#start6").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop6").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});//kadhalan
+$(document).ready(function() {
+SC.stream('/tracks/345528518',function(sound){
+$("#start7").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop7").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});//kannalanee
+$(document).ready(function() {
+SC.stream('/tracks/345495650',function(sound){
+$("#start8").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop8").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});//nilla kaaikirathu
+$(document).ready(function() {
+SC.stream('/tracks/347768843',function(sound){
+$("#start9").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop9").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
+//muthal murai
+$(document).ready(function() {
+SC.stream('/tracks/572689890',function(sound){
+$("#start10").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop10").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
+//santhana thenral
+$(document).ready(function() {
+SC.stream('/tracks/517722948',function(sound){
+$("#start11").click(function(e) {
+e.preventDefault();
+sound.start();
+});
+$("#stop11").click(function(e) {
+e.preventDefault();
+sound.stop();
+});
+});
+});
